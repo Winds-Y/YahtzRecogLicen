4fcd8cc8fdd498e0c76cabc98e5d55429018f9e8
Yanluze <Yanluze@hotmail.com>
2018-03-16 21:45:23 +0800

    make piture gray

--- YahtzRecogLicen/MainWindow.xaml.cs
+++ YahtzRecogLicen/MainWindow.xaml.cs
@@ -28,11 +28,11 @@ namespace YahtzRecogLicen
         {
             InitializeComponent();
         }
-
+        
         private void btn_open_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Jpeg文件(*.jpg)|*.jpg|Bitmap文件(*.bmp)|*.bmp|所有合适文件(*.bmp/*.jpg)|*.bmp/*.jpg";
+            openFileDialog.Filter = "Jpeg文件(*.jpg)|*.jpg|Bitmap文件(*.bmp)|*.bmp|所有合适文件(*.bmp/*.jpg)|*.bmp;*.jpg";
             openFileDialog.FilterIndex = 2;
             openFileDialog.RestoreDirectory = true;
             if (openFileDialog.ShowDialog() == true)
@@ -48,6 +48,46 @@ namespace YahtzRecogLicen
         private void btn_save_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+
+        private void btn_to_gray_Click(object sender, RoutedEventArgs e)
+        {
+            if (prime_bitmap == null)
+            {
+                MessageBox.Show("请选择图片");
+                return;
+            }
+            
+            for(int i = 0; i < prime_bitmap.Height; i++)
+            {
+                for(int j = 0; j < prime_bitmap.Width; j++)
+                {
+                    System.Drawing.Color color = prime_bitmap.GetPixel(j, i);
+                    int r = color.R;
+                    int g = color.G;
+                    int b = color.B;
+                    int x = (int)(.299 * r + .587 * g + .114 * b);
+                    //int y = color.ToArgb();
+                    //MessageBox.Show(y + "");
+                    color = System.Drawing.Color.FromArgb(x,x,x);
+                    prime_bitmap.SetPixel(j,i,color);
+                    
+                    //MessageBox.Show(color.ToString());
+                }
+            }
+            
+            IntPtr intPtr = prime_bitmap.GetHbitmap();
+            ImageSource imageSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(intPtr, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            img_load.Source = imageSource;
+            //for (int i = 0; i < prime_bitmap.Height; i++)
+            //{
+            //    for (int j = 0; j < prime_bitmap.Width; j++)
+            //    {
+            //        System.Drawing.Color color = prime_bitmap.GetPixel(j, i);
+            //        MessageBox.Show(color.ToString());
+            //    }
+            //}
         }
     }
 }
